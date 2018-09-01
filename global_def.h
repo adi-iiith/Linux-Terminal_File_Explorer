3a1dee6a031c2c56a14821ddcdd8bea90a0574ec
adi-iiith <aditya.tewari@students.iiit.ac.in>
2018-09-02 02:53:12 +0530

    updated

---
+++ global_def.h
@@ -0,0 +1,10 @@
+#include "ls.h"
+
+extern stack<string> l_arrow;
+extern stack<string> r_arrow;
+extern int mode_bit;
+extern char s[];
+extern string root;
+extern string root1;
+extern vector<string> searchs;
+extern  queue<string> sshot;
