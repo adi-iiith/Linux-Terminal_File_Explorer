adb24badcc975f020639a5cf35f9b71ee5feefe9
adi-iiith <aditya.tewari@students.iiit.ac.in>
2018-09-02 11:27:51 +0530

    added dynamic CWD and root implementation --- not system dependent --- software is protable now :)

--- module1.cpp
+++ module1.cpp
@@ -12,14 +12,31 @@
 stack<string> l_arrow;
 stack<string> r_arrow;
 int mode_bit=0;
-char s[] ="/home/adi/Downloads";
-string root = "/home/adi/";
-string root1 = "/home/adi/Downloads";
+char s[PATH_MAX+1];
+string root; 
+string root1; 
+
+// char s[] ="/home/adi/Downloads";
+// string root = "/home/adi/";
+// string root1 = "/home/adi/Downloads";
+
 queue<string>sshot;
 vector<string> searchs;
 
 int main()
 {	
+	struct passwd *pw = getpwuid(getuid());
+	const char *homedir = pw->pw_dir;                       //get home path 
+
+	char cwd[PATH_MAX+1];										// for CWD	
+	getcwd(cwd, sizeof(cwd));
+
+	strcpy(s,cwd);
+	root = homedir;
+	root1=cwd;
+
+
+
 	ls(s);
 	return 1;
 }
